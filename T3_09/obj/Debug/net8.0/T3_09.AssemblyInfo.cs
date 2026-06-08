6faf377510fff8627b505032011f1260bb2e3e00
Sandroselim <escalantesandro85@gmail.com>
2026-06-08 12:51:00 -0500

    Configurar SonarQube

--- T3_09/obj/Debug/net8.0/T3_09.AssemblyInfo.cs
+++ T3_09/obj/Debug/net8.0/T3_09.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("T3_09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597912afa998ff8521a02d8f4e5baf904d5474a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("T3_09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("T3_09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
